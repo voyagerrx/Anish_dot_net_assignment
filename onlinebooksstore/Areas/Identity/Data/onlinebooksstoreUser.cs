1b08fc86013ae3a21e964159dec7204450f48969
Dell <Dell@DESKTOP-HR0ET7N>
2025-02-14 13:03:26 +0545

    Add project files.

---
+++ onlinebooksstore/Areas/Identity/Data/onlinebooksstoreUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Identity.Client;
+
+namespace onlinebooksstore.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the onlinebooksstoreUser class
+public class onlinebooksstoreUser : IdentityUser
+{
+    public string UserName   { get; set; }
+    public string Mobile { get; set; }
+    public string State  { get; set; }
+    public string City   { get; set; }
+    public string Password { get;set; }
+}
+
---
+++ onlinebooksstore/Models/dbinitializer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using onlinebooksstore.Areas.Identity.Data;
+using onlinebooksstore.Data;
+
+namespace onlinebooksstore.Models
+{
+    public static class dninitializer
+    {
+        public static async Task InitializeAsync(onlinebooksstoreContext context, IServiceProvider serviceProvider, UserManager<onlinebooksstoreUser> userManager)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string[] roleNames = { "Admin",  "User" };
+            IdentityResult roleResult;
+            foreach (var RoleName in roleNames)
+            {
+                var roleExists = await RoleManager.RoleExistsAsync(RoleName);
+                if (!roleExists)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(RoleName));
+
+                }
+            }
+            string Email = "admin@eemc.com";
+            string Password = "Admin@123";
+            if (userManager.FindByEmailAsync(Email).Result == null)
+            {
+                onlinebooksstoreUser user = new onlinebooksstoreUser();
+                user.UserName = Email;
+                user.Email = Email;
+                IdentityResult result = userManager.CreateAsync(user, Password).Result;
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+
+
+        }
+    }
+}
